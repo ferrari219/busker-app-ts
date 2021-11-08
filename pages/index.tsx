9a26cbe4458777c8d6af6034a40917b1dd172b2e
ferrari219@nate.com <ferrari219@nate.com>
2021-11-08 23:08:21 +0900

    typescript-loader설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ const index = () => {
   return (
     <div>
       <ul>
+        <li>storybook typescript-loader설치</li>
         <li>storybook main.js변경</li>
         <li>storybook 설치</li>
         <li>svgr 설치, next.config 수정</li>
