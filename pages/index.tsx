dc0a75df7aec8878d125f980b60f962d4c30b36d
ferrari219@nate.com <ferrari219@nate.com>
2021-11-08 16:31:42 +0900

    esline.json, prettierrc, tsconfig.json 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const index = () => {
-	return (
-		<div>
-			<ul>
-				<li>prettier설치</li>
-				<li>eslint airbnb설치</li>
-				<li>nextjs 설치</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <ul>
+        <li>esline.json</li>
+        <li>prettierrc</li>
+        <li>tsconfig.json 수정</li>
+        <li>prettier설치</li>
+        <li>eslint airbnb설치</li>
+        <li>nextjs 설치</li>
+      </ul>
+    </div>
+  );
 };
 
 export default index;
