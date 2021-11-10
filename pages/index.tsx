8a836a17657678aff2f0340e917bc98b5cfbbaa1
ferrari219@nate.com <ferrari219@nate.com>
2021-11-10 20:51:28 +0900

    index수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,116 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+// import { useState } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import MapSample from "src/assets/img/map_sample.jpg";
+import FootFloat from "src/components/03_Organism/FootFloat";
+import IconButton from "src/components/02_Molecules/IconButton";
+import Header from "src/components/03_Organism/Header";
+import Nav from "src/components/04_Templates/Nav";
+import SearchResult from "src/components/03_Organism/SearchResult";
+import { SrOnly } from "src/styles/globalStyle";
 
+const h1Style = css`
+  ${SrOnly}
+`;
 const mainStyle = css`
-  background-color: black;
-  color: #fff;
+  overflow: hidden;
+  position: relative;
+  height: 100vh;
+`;
+const mapStyle = css`
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  top: 0;
+  // 포인터
+  .pointer {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    z-index: 1;
+    transform: translate(-50%, -50%);
+    transition: top ease-in-out 0.2s;
+    &.is-active {
+      top: 35%;
+    }
+  }
+  svg {
+    transform: rotate(-45deg);
+  }
+`;
+const bgmapStyle = css`
+  height: 100vh;
+  img {
+    object-fit: cover;
+  }
+`;
+const searchStyle = css`
+  position: absolute;
+  left: 0;
+  bottom: 2rem;
+  z-index: 2;
+  width: 100%;
+  padding: 0 1rem;
+`;
+const schResultStyle = css`
+  visibility: hidden;
+  height: 0;
+  opacity: 0;
+  transition: opacity ease-in-out 0.2s;
+  &.is-active {
+    visibility: visible;
+    height: auto;
+    opacity: 1;
+  }
 `;
 
-const Index = () => {
+const handleIptClick = () => {
+  const tgt = document.querySelector("#dev_search") as HTMLElement;
+  const tgt2 = document.querySelector("#dev_pointer") as HTMLElement;
+  // console.log(tgt);
+  tgt.classList.add("is-active");
+  tgt2.classList.add("is-active");
+};
+
+const Home: NextPage = () => {
   return (
-    <div css={mainStyle}>
-      <ul>
-        <li>next.config emotion관련 수정</li>
-        <li>emotion 설치</li>
-        <li>storybook preview.js</li>
-        <li>storybook typescript-loader설치</li>
-        <li>storybook main.js변경</li>
-        <li>storybook 설치</li>
-        <li>svgr 설치, next.config 수정</li>
-        <li>esline.json</li>
-        <li>prettierrc</li>
-        <li>tsconfig.json 수정</li>
-        <li>prettier설치</li>
-        <li>eslint airbnb설치</li>
-        <li>nextjs 설치</li>
-      </ul>
-    </div>
+    <section>
+      {/* https://projects.invisionapp.com/share/JZRMNHUH2BC */}
+      {/* https://61829da196f986003a313056-vzgkewuklh.chromatic.com/?path=/story/01-atom-a--overview */}
+      <Head>
+        <title>리바트몰</title>
+      </Head>
+      <main>
+        <h1 css={h1Style}>메인 페이지</h1>
+        <section css={mainStyle}>
+          <Header
+            left={
+              <>
+                <Nav />
+              </>
+            }
+          />
+          <div css={mapStyle}>
+            <div id="dev_pointer" className="pointer">
+              <IconButton icon="IcoPointer" bgColor="pointer" shadow />
+            </div>
+            <div css={bgmapStyle}>
+              <Image src={MapSample} alt="지도 샘플" />
+            </div>
+          </div>
+          <section css={searchStyle}>
+            <FootFloat iptClick={handleIptClick} />
+            <div id="dev_search" css={schResultStyle}>
+              <SearchResult />
+            </div>
+          </section>
+        </section>
+      </main>
+    </section>
   );
 };
 
-export default Index;
+export default Home;
